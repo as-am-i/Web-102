e21e7107a89d03919790e7997d08a02df6012968
asamitanii <g033.9521@gmail.com>
2017-10-20 13:57:05 -0700

    Refactoring the code and adding possible solutions

--- JS_Lab2.js
+++ JS_Lab2.js
@@ -60,6 +60,10 @@ function has23(arr3) {
 
 console.log(has23([4,5]));
 
+
+// another way of has23
+// return collection.includes(2) || collection.include(3);
+
 // 4. Given an int array length 3, 
 // if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. 
 // Return the changed array.
@@ -76,12 +80,22 @@ function fix23(arr4) {
 }
 console.log(fix23([1,2,1]));
 
+
+// another way of fix23
+
+// for (let index = 0; index < arr4.length-1; index++) {
+// 	if(arr4[index] == 2 && arr4[index+1] == 3) {
+// 		arr4[index+1] == 0;
+// 	}
+// }
+
+
 // 5. Given an array of ints of odd length, 
 // look at the first, last, and middle values in the array and return the largest. 
 // The array length will be a least 1.
 function maxTriple(arr5) {
-	var first = arr5.shift();
-	var last = arr5.pop();
+	var first = arr5.shift(); // arr5[0]
+	var last = arr5.pop(); //arr5[arr5.size -1]
 	var middle = arr5[Math.floor(arr5.length / 2)];
 
 	if (arr5.length >= 1) {
@@ -96,9 +110,11 @@ function maxTriple(arr5) {
 // 6. Given an array of ints, swap the first and last elements in the array. 
 // Return the modified array. The array length will be at least 1.
 function swapEnds(arr6) {
-	var first = arr6.shift();
-	var last = arr6.pop();
 	if (arr6.length >= 1) {
+
+		var first = arr6.shift();
+		var last = arr6.pop();
+
 		arr6.unshift(last);
 		arr6.push(first);
 		return arr6;
@@ -120,6 +136,23 @@ function unlucky1(arr7) {
 		return false;
 	}
 }
+
+
+// another way of arr7
+
+// function unlucky1(collection) {    
+//     var last = collection.length - 2;
+  
+//     if((collection[0] = 1 && collection[1] == 3) || 
+//        (collection[1] = 1 && collection[2] == 3) || 
+//        (collection[last] = 1 && collection[last + 1] == 3) ) {
+      
+//       return true;
+//     }   
+  
+//     return false;
+// }
+
 
 // 8. Given 2 int arrays, a and b, of any length, 
 // return a new array with the first element of each array. 
@@ -180,6 +213,19 @@ function isEverywhere(arr9, value) {
 	}
 }
 
+// another way of isEverywhere(arr9, value)
+
+// var size = arr9.length - 1
+// for (let index = 0; index < size; index++) {
+// 	if (arr9[index] != value && arr9[index+1] != value) {
+// 		return false;
+// 		}
+// 	else {
+// 		return true;
+// 	}
+// }
+
+
 // 10. For each multiple of 10 in the given array, 
 // change all the values following it to be that multiple of 10, 
 // until encountering another multiple of 10. 
@@ -231,6 +277,30 @@ function twoTwo(arr11) {
     }
     return true;
 }
+
+// another way of twoTwo(arr11)
+
+// function twoTwo(collection) {
+// 	var count = 0;
+    
+// 	for(let index = 0; index < collection.length; index++) {
+// 		if(collection[index] == 2){
+// 			count++;
+//         }
+// 		else {
+// 			if(count == 1){
+// 				return false;
+//             }
+// 			else{
+// 				count = 0;
+//             }
+// 		}
+// 	}
+    
+// 	return (count != 1);
+// }
+
+
 
 // 12. Return an array that is "left shifted" by one
 // so {6, 2, 5, 3} returns {2, 5, 3, 6}. 
@@ -353,4 +423,27 @@ function countClumps(arr15) {
 }
 
 countClumps([1,2,2,2,4])
+
+
+// another way of countClumps(arr15)
+
+// function countClumps(collection) {    
+//     var clumps = 0;
+//     var isClump = false;
+//     var size = collection.length - 1; 
+    
+//     for(let index = 0; index < size; index++){
+//         if(isClump){
+//             if(collection[index] != collection[index+1]){
+//                 isClump = false;
+//             }
+//         }
+//         else if(collection[index] == collection[index+1]){
+//             isClump = true;
+//             clumps++;
+//         }
+//     }
+    
+//     return clumps;    
+// }
 
